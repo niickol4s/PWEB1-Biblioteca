32fc90cc405a4152e08716f164ef602d4bfa924e
Nickolas Davi <105878022+niickol4s@users.noreply.github.com>
2024-07-29 14:52:58 -0300

    Add files via upload

---
+++ biblioteca/Pessoa.php
@@ -0,0 +1,19 @@
+<?php
+    class Pessoa {
+        private $nome;
+        private $cpf;
+
+        public function __construct($nome, $cpf) {
+            $this->nome = $nome;
+            $this->cpf = $cpf;
+        }
+
+        public function getNome() {
+            return $this->nome;
+        }
+
+        public function getCpf() {
+            return $this->cpf;
+        }
+    } 
+?>
